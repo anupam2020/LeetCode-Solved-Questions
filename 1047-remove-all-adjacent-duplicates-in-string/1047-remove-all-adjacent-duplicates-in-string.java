670eb338cc5dba3873efe87bbf85ae23a6f1de9f
Anupam Basak <63058877+anupam2020@users.noreply.github.com>
2022-11-10 10:12:09 +0530

    Time: 25 ms (82.75%), Space: 50 MB (62.28%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -5,9 +5,15 @@ class Solution {
             // if the current character is same as the last one in `sb`
             // then we cannot push it to `sb`
             // we remove the one in `sb`
-            if (sb.length() > 0 && sb.charAt(sb.length() - 1) == c) sb.deleteCharAt(sb.length() - 1);
+            if (sb.length() > 0 && sb.charAt(sb.length() - 1) == c) 
+            {
+                sb.deleteCharAt(sb.length() - 1);
+            }
             // otherwise, add the current character to `sb`
-            else sb.append(c);
+            else
+            {
+                sb.append(c);
+            }
         }
         return sb.toString();
     }
